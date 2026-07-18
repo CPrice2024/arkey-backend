c9789d7a975ce443e7f929f108b9b74e5e81d43b
CPrice2024 <redhamikael@gmail.com>
2026-07-18 12:31:42 +0300

    debug test transaction 11

---
+++ locales/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  en: {
+    welcome: "Welcome",
+    register: "✅ Register",
+    balance: "◈ Balance",
+    deposit: "◉ Deposit",
+    withdraw: "⇧ Withdraw",
+    play: "▣ Play Game",
+    language: "🌍 Language",
+    support: "◍ Support",
+  },
+
+  am: {
+    welcome: "እንኳን ደህና መጡ",
+    register: "✅ ይመዝገቡ",
+    balance: "◈ ቀሪ ሂሳብ",
+    deposit: "◉ ገንዘብ አስገባ",
+    withdraw: "⇧ ገንዘብ አውጣ",
+    play: "▣ ጨዋታ ጀምር",
+    language: "🌍 ቋንቋ",
+    support: "◍ ድጋፍ",
+  },
+
+  om: {
+    welcome: "Baga Nagaan Dhufte",
+    register: "✅ Galmaa'i",
+    balance: "◈ Baalansii",
+    deposit: "◉ Maallaqa Galchi",
+    withdraw: "⇧ Maallaqa Baasi",
+    play: "▣ Tapha Jalqabi",
+    language: "🌍 Afaan",
+    support: "◍ Deeggarsa",
+  },
+};
